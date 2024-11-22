4a9486faa5d4dbf8668b0e6fedc925db52c2e5b0
Aakanksha-28 <aakankshathokare28@gmail.com>
2024-11-22 12:42:59 +0530

    added new line

--- add.jsx
+++ add.jsx
@@ -3,6 +3,6 @@ const num2 = 3;
 
 // add two numbers
 const sum = num1 + num2;
-
+console.log("testing if it just showing the difference");
 // display the sum
 console.log("The sum of " + num1 + " and " + num2 + " is: " + sum);
